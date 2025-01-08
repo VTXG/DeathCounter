a05e503dc3a42fa8ec1e9cc33600380e50bd18c0
VTXG <VTXG@users.noreply.github.com>
2025-01-08 20:53:01 +0000

    PlayerMissLeft Changed

--- source/DeathCounter.cpp
+++ source/DeathCounter.cpp
@@ -17,10 +17,20 @@ s32 DeathCounterSystem::getMissCount() {
     return 0;
 }
 
-kmCall(0x80487140+REGIONOFF, DeathCounterSystem::getMissCount); // PauseMenu::appear
-kmCall(0x80488D2C+REGIONOFF, DeathCounterSystem::getMissCount); // PlayerLeft::init
-kmCall(0x80488EE8+REGIONOFF, DeathCounterSystem::getMissCount); // PlayerLeft::exeHide
-kmCall(0x80488FE0+REGIONOFF, DeathCounterSystem::getMissCount); // PlayerLeft::exeWait
-kmCall(0x80489038+REGIONOFF, DeathCounterSystem::getMissCount); // PlayerLeft::exeWait
-kmCall(0x8048EB1C+REGIONOFF, DeathCounterSystem::getMissCount); // ScenarioSelectLayout::setPlayerLeft
-kmCall(0x8048EB30+REGIONOFF, DeathCounterSystem::getMissCount); // ScenarioSelectLayout::setPlayerLeft
+// PauseMenu
+kmCall(0x80487140+REGIONOFF, DeathCounterSystem::getMissCount); // ::appear
+
+// PlayerLeft
+kmCall(0x80488D2C+REGIONOFF, DeathCounterSystem::getMissCount); // ::init
+kmCall(0x80488EE8+REGIONOFF, DeathCounterSystem::getMissCount); // ::exeHide
+kmCall(0x80488FE0+REGIONOFF, DeathCounterSystem::getMissCount); // ::exeWait
+kmCall(0x80489038+REGIONOFF, DeathCounterSystem::getMissCount); // ::exeWait
+
+// PlayerMissLeft
+kmCall(0x80489424+REGIONOFF, DeathCounterSystem::getMissCount); // ::exeAppear
+kmWrite32(0x80489434+REGIONOFF, 0x38A5FFFF); // ::exeAppear (addi r5, r5, -1)
+kmCall(0x80489514+REGIONOFF, DeathCounterSystem::getMissCount); // ::exeWait
+
+// ScenarioSelectLayout
+kmCall(0x8048EB1C+REGIONOFF, DeathCounterSystem::getMissCount); // ::setPlayerLeft
+kmCall(0x8048EB30+REGIONOFF, DeathCounterSystem::getMissCount); // ::setPlayerLeft
